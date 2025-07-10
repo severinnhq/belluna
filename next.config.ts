3dcab1d2e044714f52f918124b956cdf3a79238f
severinnhq <sstierbusiness@gmail.com>
2025-07-10 14:17:59 +0200

    ready

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-   experimental: {
-  optimizePackageImports: [],
-},
-  output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  // Windows-specific path fixes 
   webpack: (config) => {
     config.watchOptions = {
       poll: 1000,
