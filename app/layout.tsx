71cb64e5e310ba41780f8aba2eebd5b97ec04eae
severinnhq <sstierbusiness@gmail.com>
2025-07-21 21:17:15 +0200

    ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,23 +3,21 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import PixelClient from "@/components/PixelClient";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "BellunaDigital | Új páciensek plasztikai sebészeteknek, automatizáltan",
-  description: "",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="select-none">
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-        <base href="/" />
-      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         <PixelClient />
