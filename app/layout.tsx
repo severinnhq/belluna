54ab7622dec6fc1dcf88be1ae2f9a815b2af3c0b
severinnhq <sstierbusiness@gmail.com>
2025-07-21 20:41:33 +0200

    ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import PixelClient from "@/components/PixelClient";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
