55c09ff03f7ac8b70243f1211e61e09e7baf5917
severinnhq <sstierbusiness@gmail.com>
2025-07-26 20:33:12 +0200

    ready

--- components/PixelClient.tsx
+++ components/PixelClient.tsx
@@ -9,7 +9,6 @@ export default function PixelClient() {
     console.warn("Meta Pixel ID is missing. Set NEXT_PUBLIC_PIXEL_ID in your environment.");
     return null;
   }
-  console.log("PIXEL ID:", pixelId);
 
   return (
     <>
@@ -19,14 +18,29 @@ export default function PixelClient() {
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
+            !(function(f, b, e, v, n, t, s) {
+              if (f.fbq) return;
+              n = f.fbq = function() {
+                n.callMethod
+                  ? n.callMethod.apply(n, arguments)
+                  : n.queue.push(arguments);
+              };
+              if (!f._fbq) f._fbq = n;
+              n.push = n;
+              n.loaded = true;
+              n.version = '2.0';
+              n.queue = [];
+              t = b.createElement(e);
+              t.async = true;
+              t.src = v;
+              s = b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t, s);
+            })(
+              window,
+              document,
+              'script',
+              'https://connect.facebook.net/en_US/fbevents.js'
+            );
             fbq('init', '${pixelId}');
             fbq('track', 'PageView');
           `,
