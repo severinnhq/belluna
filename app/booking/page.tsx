43aaed609dd89da3bed9f8112e2823c091ace2cf
severinnhq <sstierbusiness@gmail.com>
2025-08-03 18:29:23 +0200

    ready

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -7,20 +7,21 @@ const BookingPage: React.FC = () => {
   return (
     <main className="flex flex-col items-center p-8 bg-gray-50 min-h-screen">
       <h1 className="text-4xl text-black font-bold mb-4">Foglaljon időpontot!</h1>
-      
+
       {/* Wider Container */}
       <div className="w-full max-w-6xl mb-6">
         <iframe
           src="https://api.leadconnectorhq.com/widget/booking/RxkYNdkRcOBFB3Ce6kdj"
-          style={{ width: '100%', border: 'none', overflow: 'hidden', minHeight: '800px' }}
+          style={{ width: '100%', border: 'none', overflow: 'hidden' }}
           scrolling="no"
-          id="RxkYNdkRcOBFB3Ce6kdj_1754236207558"
+          id="RxkYNdkRcOBFB3Ce6kdj_1754238548924"
           title="Foglaljon időpont widget"
         ></iframe>
       </div>
 
       <Script
         src="https://link.msgsndr.com/js/form_embed.js"
+        type="text/javascript"
         strategy="lazyOnload"
       />
 
